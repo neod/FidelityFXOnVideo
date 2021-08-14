10df57842e5be1cbb5824c8e13b8af2a38ab3ab4
Neod <neod@live.be>
2021-08-14 21:53:30 +0200

    clean unused using

--- FidelityFXOnVideo/FidelityFXOnVideo.cs
+++ FidelityFXOnVideo/FidelityFXOnVideo.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using FFMpegCore;
 
 namespace FidelityFXOnVideo
